d682a495d831496a8be8306dbc888d2aaba677bd
bobosii <day.emir@hotmail.com>
2026-01-23 07:30:18 +0300

    implemet cors configs and prettier
    Signed-off-by: bobosii <day.emir@hotmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-import express from 'express';
+import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use("/api/subjects", subjectsRouter);
 
-app.get('/', (req, res) => {
-    res.send("Hello from backend");
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at localhost:${PORT}`)
+  console.log(`Server is running at localhost:${PORT}`);
 });
